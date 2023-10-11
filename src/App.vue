036bf33525082f8805967502b788b704d29a080b
Emi <Emi@DESKTOP-03D2CE7>
2023-10-10 21:23:25 -0300

    proyecto nuevo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+let mensajes
+
+const buscarMensaje  = () => {
+  fetch('http://localhost:3000/')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La solicitud falló');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Maneja los datos recibidos del servidor
+    mensajes=data
+    
+  })
+  .catch(error => {
+    // Maneja errores
+    console.error(error);
+  });
 }
+
+buscarMensaje()
+console.log(mensajes)
+
 </script>
 
+<template>
+  <div>
+    <!-- Itera sobre los twits -->
+    <div v-for="twit in mensajes" :key="twit">
+      <div>
+        <p>ID: {{ twit.id }}</p>
+        <p>Usuario: {{ twit.user }}</p>
+        <p>Mensaje: {{ twit.message }}</p>
+        <p>Timestamp: {{ twit.timestamp }}</p>
+      </div>
+
+      <!-- Itera sobre las respuestas -->
+      <div v-for="response in mensajes.twit.responses" :key="response.id">
+        <div>
+          <p>Respuesta ID: {{ response.id }}</p>
+          <p>Usuario: {{ response.user }}</p>
+          <p>Mensaje: {{ response.message }}</p>
+          <p>Timestamp: {{ response.timestamp }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
